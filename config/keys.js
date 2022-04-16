36359dc0b38da51e7dd874dd1bb8372157bda88a
ms34 <33662819+ms34@users.noreply.github.com>
2022-04-16 12:00:53 +0300

    updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     // mongoURI: 'mongodb://localhost:27017/sh-list'
-    mongoURI: 'mongodb+srv://mak:mak@cluster0-oscfe.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: ''
 }
